467afe9421f6f40927ec096c893734c19a051658
szarp <lizusfull@gmail.com>
2016-09-11 23:47:21 +0200

    script for encrypt substitution from page

--- webPage/learn-set.js
+++ webPage/learn-set.js
@@ -3,13 +3,80 @@ var mainEvents ={
     'home':['navbar_home','choseMenu'],
     'newGame':['newgame','gameArea']
 }
+var gcode = [[0,4.3],[3.6,6.4],[6.4,4.7],[7.9,1],[6.1,-4],[3.6,-5.9],[0,-7.9],[-3.6,-5.9],[-6.1,-4],[-7.9,1],[-6.4,4.7],[-3.6,6.4],[0,4.3]];
+var commands="";
+function addCommand(comand){
+    commands+=comand+'\n';
+    
+}
+function newFunc(){
+    var newPoint=[];
+    for(var i=0;i<gcode.length;i++){
+        newPoint=gcode[i];
+        addCommand('G1 x' +newPoint[0]+' y'+newPoint[1]);
+    }
+}
+function createOutLine(){
+    var beginPoint=[];
+    var endPoint=[];
+    var newPoint=[];
+    var l=gcode.length;
+    for(var i=0 ;i<gcode.length-1;i++){
+        beginPoint=gcode[i];
+        endPoint=gcode[i+1];
+        newPoint[0]=endPoint[0]-beginPoint[0];
+        newPoint[1]=endPoint[1]-beginPoint[1];
+        newPoint[0]=Math.floor(newPoint[0]*10)/5;
+        newPoint[1]=Math.floor(newPoint[1]*10)/5;
+        //var w=Math.floor(newPoint[0]*100)/50;
+        addCommand('G1 x' +newPoint[0]+' y'+newPoint[1]);
+        //console.log(w);
+    }
+    beginPoint=gcode[1];
+    endPoint=gcode[l-1];
+        newPoint[0]=endPoint[0]-beginPoint[0];
+        newPoint[1]=endPoint[1]-beginPoint[1];
+        newPoint[0]=Math.floor(newPoint[0]*10)/5;
+        newPoint[1]=Math.floor(newPoint[1]*10)/5;
+        addCommand('G1 x' +newPoint[0]+' y'+newPoint[1]);
+        addCommand("G1 x0 y-8.6")
+}
+function createInLine(){
+        var beginPoint;
+    var endPoint;
+    var newPoint=[];
+    var l=gcode.length;
+    for(var i=1 ;i<gcode.length-1;i++){
+        beginPoint=2*gcode[i][0];
+        endPoint=2*gcode[i][1];
+        addCommand('G1 x' +beginPoint+' y'+endPoint);
+        addCommand('G1 x' +reverseNumber(beginPoint)+' y'+reverseNumber(endPoint));
+        //console.log(w);
+    }
+    
+}
+function allComnads(){
+    //createOutLine();
+    //createInLine();
+    newFunc();
+    console.log(commands);    
+}
+function reverseNumber(a){
+ 
+    return -a;
+}
 
-
+function newGameSession(){
+    x.clearTable();
+    x.onStart();
+    x.onEnd();
+    
+}
 function testd(){
        x.setStartMode();
     for (k in mainEvents){
         //console.log(mainEvents[k]);
-        addEvent(mainEvents[k]);
+        changeDisplayOnClick(mainEvents[k]);
     }
     console.log('hh');
 }
@@ -32,7 +99,7 @@ function goTo(id){
     }
     
 }
-function addEvent(key){
+function changeDisplayOnClick(key){
     console.log(key[0]);
     
     var el = document.getElementById(key[0]);
@@ -41,6 +108,7 @@ function addEvent(key){
 }
 function onStartTable(){
     x.onStart();
+    x.nextRound();
     //console.log('kk');
 }
 function onEventTable(){
---
+++ webPage/substitutionScript.js
@@ -0,0 +1,304 @@
+
+var changesList=[];
+function asd(){
+    var x=new getSubstitution();
+    x.creatKeyList();
+    x.allKeyElements();
+    x.changeIdForElements();
+    //console.log(x.keyElements);
+    
+    
+}
+
+
+function getSubstitution(){
+    this.keyList={};
+    this.substitutionList={};
+    this.keyElements=[];
+    this.changeIdForElements=function(){
+    for (var i=0;i<this.dataArray.length;i++){
+        var actualChange=this.dataArray[i];
+        for (k in actualChange){
+            if (k == 'changes'){
+                var toChange = actualChange[k];
+                for(var i=0; i<toChange.length;i++){
+                    for (l in toChange[i]){
+                        console.log(l +"  "+toChange[i][l]);
+
+                    }
+                }
+
+            }
+            else{
+                console.log(k +"  "+this.dataArray[k]);
+            }
+        }
+    }
+    
+}
+
+this.findInCatgory=function(category,element){
+    var findingList=keyLisit[category];
+    for(var i=0;i<findingList.length;i++){
+        for (k in findingList[i]){
+            if(k==element){
+                return findingList[i][k];
+            }
+        }
+    }
+    throw('invalid element or category in function findInCategory');  
+}
+    this.allKeyElements=function(){
+        var i=0
+        for(k in this.keyList){
+            this.keyElements[i]=k;
+            //console.log(this.keyList);
+            i++;
+        }
+    }
+this.returnCategoryName=function(string){
+    for(var i=0;i<this.keyElements.length;i++){
+        
+        var ifContain=this.include(string,this.keyElements[i]);
+        if(ifContain!=-1){return ifContain};
+    }
+    
+}
+this.include=function(arr,obj) {
+    return (arr.indexOf(obj) != -1);
+}
+this.creatKeyList=function(){
+    for (k in this.keyArray){ //techer
+        var category=this.keyArray[k];
+        var num=0;
+            this.keyList[k] = [];
+        for(l in category){
+            var element=category[l]
+            var newObject={};
+            newObject[element['id']] = element['short'];
+             this.keyList[k][num]=newObject;
+            num++;
+            
+        }
+
+    }
+    //console.log(keyLsit);
+}
+
+this.keyArray = {"teachers":{
+"75":{"id":75,"name":"Pindur Zofia","short":"Pindur"},
+"78":{"id":78,"name":"Stankowska Urszula","short":"Stankowska"},
+"98":{"id":98,"name":"Cwołek Agnieszka","short":"Cwołek"},
+"-7":{"id":-7,"name":"Wlisłocka Małgorzata","short":"Wlisłocka Małgorzata"},
+"101":{"id":101,"name":"Glombik Artur","short":"Glombik"},
+"108":{"id":108,"name":"Kucharz Ewelina","short":"Kucharz"},
+"111":{"id":111,"name":"Rabsztyn Barbara","short":"Rabsztyn"},
+"122":{"id":122,"name":"Mataczyński Grzegorz","short":"Mataczynski"},
+"124":{"id":124,"name":"Sazanów Jolanta","short":"Sazanow"},
+"-15":{"id":"-15","name":"Wiśniowicz Artur","short":"Wiśniowicz Artur"}},
+"classes":{
+"AB8383A5E60E7751":{"id":"AB8383A5E60E7751","name":"1gd","short":"1gd"},
+"1B141C680F246401":{"id":"1B141C680F246401","name":"1a","short":"1a"},
+"2891875F58158DD0":{"id":"2891875F58158DD0","name":"2a","short":"2a"},
+"483A06429BAC5832":{"id":"483A06429BAC5832","name":"2b","short":"2b"},
+"26E141B0DC950D93":{"id":"26E141B0DC950D93","name":"2c","short":"2c"},
+"3CEA346CB6726060":{"id":"3CEA346CB6726060","name":"2d","short":"2d"},
+"C28E85A4B422647C":{"id":"C28E85A4B422647C","name":"3a","short":"3a"}},
+"subjects":{
+"35":{"id":35,"name":"język polski","short":"polski"},
+"40":{"id":40,"name":"matematyka","short":"matematyka"},
+"45":{"id":45,"name":"biologia","short":"biologia"},
+"47":{"id":47,"name":"fizyka","short":"fizyka"},
+"54":{"id":54,"name":"wf","short":"wf"},
+"89D438FE7417313E":{"id":"89D438FE7417313E","name":"sih 3","short":"sih 3"},
+"D674369697EFC0DE":{"id":"D674369697EFC0DE","name":"matematyka 8","short":"matematyka 8"},
+"02138B763A306E02":{"id":"02138B763A306E02","name":"sih 8","short":"sih 8"}},
+"classrooms":{
+"4":{"id":4,"name":"5","short":"5"},
+"8B82690C1E6C5BA0":{"id":"8B82690C1E6C5BA0","name":"9","short":"9"},
+"85F7E7BFF7DE1CDD":{"id":"85F7E7BFF7DE1CDD","name":"10","short":"10"},
+"5851B116FFF07778":{"id":"5851B116FFF07778","name":"21","short":"21"},
+"628D6B2D80ED0133":{"id":"628D6B2D80ED0133","name":"32","short":"32"},
+"083B4B00D8AA754E":{"id":"083B4B00D8AA754E","name":"46","short":"46"},
+"A1F143F9F27783BE":{"id":"A1F143F9F27783BE","name":"0 gim","short":"0gim"}},
+"periods":[
+{"name":"1","short":"1","starttime":"07:25","endtime":"08:10","id":"0"},
+{"name":"2","short":"2","starttime":"08:15","endtime":"09:00","id":"1"},
+{"name":"3","short":"3","starttime":"09:10","endtime":"09:55","id":"2"},
+{"name":"4","short":"4","starttime":"10:05","endtime":"10:50","id":"3"},
+{"name":"5","short":"5","starttime":"11:00","endtime":"11:45","id":"4"},
+{"name":"6","short":"6","starttime":"12:05","endtime":"12:50","id":"5"},
+{"name":"7","short":"7","starttime":"13:00","endtime":"13:45","id":"6"},
+{"name":"8","short":"8","starttime":"13:55","endtime":"14:40","id":"7"}],
+"absent_types":{
+"5DCBE154994C7BB1":{"id":"5DCBE154994C7BB1","name":"Urlop okolicznościowy","short":"Urlop okolicznościowy"},
+"81CEF962273F7C6D":{"id":"81CEF962273F7C6D","name":"wyjście z klasą","short":"wyjście z klasą"}},
+"substitution_types":{
+"E86C1B744DD94653":{"id":"E86C1B744DD94653","name":"płatne","short":"płatne"}}}
+
+
+
+
+
+
+
+this.dataArray=
+[{"cancelled":true,"note":"","substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":54},
+{"column":"teacherids","old":124},
+{"column":"classids","old":"1B141C680F246401"},
+{"column":"classroomids","old":"A1F143F9F27783BE"}],"period":"0","subjectid":54,
+"teacherids":[124],
+"classids":["1B141C680F246401"],
+"classroomids":["A1F143F9F27783BE"],
+"groupnames":["Chłopcy"],"type":"card","periodorbreak":"00P","moje":false},
+{"cancelled":true,"substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":35},
+{"column":"teacherids","old":75},
+{"column":"classids","old":"AB8383A5E60E7751"},
+{"column":"classroomids","old":"5851B116FFF07778"}],"period":"5","subjectid":35,
+"teacherids":[75],
+"classids":["AB8383A5E60E7751"],
+"classroomids":["5851B116FFF07778"],
+"groupnames":[""],"type":"card","periodorbreak":"05P","moje":false},
+{"cancelled":true,"substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":35},
+{"column":"teacherids","old":75},
+{"column":"classids","old":"AB8383A5E60E7751"},
+{"column":"classroomids","old":"5851B116FFF07778"}],"period":"6","subjectid":35,
+"teacherids":[75],
+"classids":["AB8383A5E60E7751"],
+"classroomids":["5851B116FFF07778"],
+"groupnames":[""],"type":"card","periodorbreak":"06P","moje":false},
+{"cancelled":true,"substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":40},
+{"column":"teacherids","old":98},
+{"column":"classids","old":"AB8383A5E60E7751"},
+{"column":"classroomids","old":"8B82690C1E6C5BA0"}],"period":"7","subjectid":40,
+"teacherids":[98],
+"classids":["AB8383A5E60E7751"],
+"classroomids":["8B82690C1E6C5BA0"],
+"groupnames":[""],"type":"card","periodorbreak":"07P","moje":false},
+{"cancelled":true,"substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":45},
+{"column":"teacherids","old":108},
+{"column":"classids","old":"AB8383A5E60E7751"},
+{"column":"classroomids","old":"85F7E7BFF7DE1CDD"}],"period":"2","subjectid":45,
+"teacherids":[108],
+"classids":["AB8383A5E60E7751"],
+"classroomids":["85F7E7BFF7DE1CDD"],
+"groupnames":["1. Grupa"],"type":"card","periodorbreak":"02P","moje":false},
+{"cancelled":true,"substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":45},
+{"column":"teacherids","old":108},
+{"column":"classids","old":"AB8383A5E60E7751"},
+{"column":"classroomids","old":"85F7E7BFF7DE1CDD"}],"period":"1","subjectid":45,
+"teacherids":[108],
+"classids":["AB8383A5E60E7751"],
+"classroomids":["85F7E7BFF7DE1CDD"],
+"groupnames":["2. Grupa"],"type":"card","periodorbreak":"01P","moje":false},
+{"cancelled":true,"substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":47},
+{"column":"teacherids","old":111},
+{"column":"classids","old":"AB8383A5E60E7751"},
+{"column":"classroomids","old":"083B4B00D8AA754E"}],"period":"1","subjectid":47,
+"teacherids":[111],
+"classids":["AB8383A5E60E7751"],
+"classroomids":["083B4B00D8AA754E"],
+"groupnames":["1. Grupa"],"type":"card","periodorbreak":"01P","moje":false},
+{"cancelled":true,"substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":47},
+{"column":"teacherids","old":111},
+{"column":"classids","old":"AB8383A5E60E7751"},
+{"column":"classroomids","old":"083B4B00D8AA754E"}],"period":"2","subjectid":47,
+"teacherids":[111],
+"classids":["AB8383A5E60E7751"],
+"classroomids":["083B4B00D8AA754E"],
+"groupnames":["2. Grupa"],"type":"card","periodorbreak":"02P","moje":false},
+{"cancelled":true,"note":"","substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":54},
+{"column":"teacherids","old":124},
+{"column":"classids","old":"1B141C680F246401"},
+{"column":"classroomids","old":"A1F143F9F27783BE"}],"period":"1","subjectid":54,
+"teacherids":[124],
+"classids":["1B141C680F246401"],
+"classroomids":["A1F143F9F27783BE"],
+"groupnames":["Chłopcy"],"type":"card","periodorbreak":"01P","moje":false},
+{"cancelled":true,"note":"","substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":54},
+{"column":"teacherids","old":124},
+{"column":"classids","old":"C28E85A4B422647C"},
+{"column":"classroomids","old":"A1F143F9F27783BE"}],"period":"2","subjectid":54,
+"teacherids":[124],
+"classids":["C28E85A4B422647C"],
+"classroomids":["A1F143F9F27783BE"],
+"groupnames":["Dziewczęta"],"type":"card","periodorbreak":"02P","moje":false},
+    
+{"cancelled":false,"substitution_typeid":"E86C1B744DD94653","note":"",
+"changes":[
+{"column":"teacherids","old":101,"new":78}],"period":"3","subjectid":"89D438FE7417313E",
+"teacherids":[101],
+"classids":["2891875F58158DD0",
+"3CEA346CB6726060"],
+"classroomids":[4],
+"groupnames":["seminargroup:3"],"type":"card","periodorbreak":"03P","moje":false},
+{"cancelled":false,"substitution_typeid":"E86C1B744DD94653","note":"",
+"changes":[
+{"column":"teacherids","old":101,"new":-7},
+{"column":"subjectid","old":"02138B763A306E02","new":"D674369697EFC0DE"}],"period":"4","subjectid":"02138B763A306E02",
+"teacherids":[101],
+"classids":["2891875F58158DD0",
+"483A06429BAC5832","26E141B0DC950D93","3CEA346CB6726060"],
+"classroomids":["628D6B2D80ED0133"],
+"groupnames":["seminargroup:1"],"type":"card","periodorbreak":"04P","moje":false},
+{"cancelled":true,"substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":54},
+{"column":"teacherids","old":"-15"},
+{"column":"classids","old":"AB8383A5E60E7751"},
+{"column":"classroomids","old":"A1F143F9F27783BE"}],"period":"3","subjectid":54,
+"teacherids":["-15"],
+"classids":["AB8383A5E60E7751"],
+"classroomids":["A1F143F9F27783BE"],
+"groupnames":["Chłopcy"],"type":"card","periodorbreak":"03P","moje":false},
+{"cancelled":true,"substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":54},
+{"column":"teacherids","old":"-15"},
+{"column":"classids","old":"AB8383A5E60E7751"},
+{"column":"classroomids","old":"A1F143F9F27783BE"}],"period":"4","subjectid":54,
+"teacherids":["-15"],
+"classids":["AB8383A5E60E7751"],
+"classroomids":["A1F143F9F27783BE"],
+"groupnames":["Chłopcy"],"type":"card","periodorbreak":"04P","moje":false},
+{"cancelled":true,"substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":54},
+{"column":"teacherids","old":122},
+{"column":"classids","old":"AB8383A5E60E7751"},
+{"column":"classroomids","old":"A1F143F9F27783BE"}],"period":"3","subjectid":54,
+"teacherids":[122],
+"classids":["AB8383A5E60E7751"],
+"classroomids":["A1F143F9F27783BE"],
+"groupnames":["Dziewczęta"],"type":"card","periodorbreak":"03P","moje":false},
+{"cancelled":true,"substitution_typeid":"",
+"changes":[
+{"column":"subjectid","old":54},
+{"column":"teacherids","old":122},
+{"column":"classids","old":"AB8383A5E60E7751"},
+{"column":"classroomids","old":"A1F143F9F27783BE"}],"period":"4","subjectid":54,
+"teacherids":[122],
+"classids":["AB8383A5E60E7751"],
+"classroomids":["A1F143F9F27783BE"],
+"groupnames":["Dziewczęta"],"type":"card","periodorbreak":"04P","moje":false}]
+}
