b067e00833d5917c6bc47d27d18c6296004468cf
szarp <lizusfull@gmail.com>
2016-09-29 23:08:05 +0200

    cut code to modules

---
+++ substitution/myModules/setDate.js
@@ -0,0 +1,39 @@
+
+module.exports = function setDate(){
+    //this.Today = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+    //this.day
+    //this.month
+    //this.year
+    this.dispalyTime=function(){
+        return this.year+'-'+this.month+'-'+this.day;
+    }
+    this.updateTime=function(){
+        this.year = this.Today.getFullYear();
+        this.month = this.Today.getMonth()+1;
+        this.day = this.Today.getDate();
+        if(this.month<10){this.month='0'+this.month;};
+        if(this.day<10){this.day='0'+this.day;};
+    }
+    this.todayIs=function(){
+        this.Today = new Date();
+        this.updateTime();
+
+    }
+    this.tommorowIS=function(){
+        this.Today = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+        this.updateTime();
+    }
+    this.yeasterdayIS=function(){
+        this.Today = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
+        this.updateTime();
+    }
+    this.setDate=function(year,month,day){
+        this.Today=new Date();
+        this.Today.setFullYear(year);
+        this.Today.setDate(day);
+        this.Today.setMonth(month);
+        this.updateTime();
+        
+    }
+    
+}
